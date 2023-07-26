33d52d0b02ab98f2080bb09335a59e578e01e10c
julie-mac <123594716+julie-mac@users.noreply.github.com>
2023-07-25 22:15:52 -0400

    initiated react app with base code

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectCard = (props) => {
+    return (
+        <div className="card">
+            
+        </div>
+     )
+}
+
+export default ProjectCard;
---
+++ src/components/ProjectInfo.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import ProjectCard from './ProjectCard';
+
+const ProjectInfo = (props) => {
+    const [project, setProject] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        
+    })
+
+}
